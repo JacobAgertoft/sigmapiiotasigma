e4f25a5ab6f3a738c31b00a2b612576660cd8746
Jacob Agertoft <jacob.agertoft@hotmail.com>
2022-11-16 18:36:28 -0500

    Add swap style for all files

--- js/StyleSwap.js
+++ js/StyleSwap.js
@@ -4,10 +4,10 @@ document.getElementById("btn2").onclick = function() {SwapStyle()};
 
 function SwapStyle() {
     if (active_stylesheet == "main") {
-        document.getElementById("pagestyle").setAttribute("href", "alternative.css");  
+        document.getElementById("style").setAttribute("href", "alternative.css");  
         active_stylesheet = "alternative";
     } else {
-        document.getElementById("pagestyle").setAttribute("href", "main.css"); 
+        document.getElementById("style").setAttribute("href", "main.css"); 
         active_stylesheet = "main";
     }
 }
